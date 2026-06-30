fb4b45560e6712f7bdce98c606256eb70d62516b
Paul Abbott <phatpaul@gmail.com>
2026-06-30 18:21:46 -0400

    my_lpc845_wip test code

--- examples/BlinkLPC845-PlatformIO/src/main.cpp
+++ examples/BlinkLPC845-PlatformIO/src/main.cpp
@@ -2,9 +2,9 @@
 #include <stdint.h>
 
 #define LED_PIN 0
-#define NUM_LEDS 2
+#define NUM_LEDS 4
 #define BRIGHTNESS 255
-#define LED_TYPE WS2812
+#define LED_TYPE WS2811
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
 
@@ -15,28 +15,39 @@ void wait_for_serial_connection() {
     } // wait until the connection to the PC is established
 }
 
+void nextColor() {
+    static uint8_t colorIndex = 0;
+    colorIndex = (colorIndex + 1) % 3; // Cycle through 3 colors
+
+    for (int i = 0; i < NUM_LEDS; ++i) {
+        switch (colorIndex) {
+            case 0:
+                leds[i] = CRGB::Red;
+                break;
+            case 1:
+                leds[i] = CRGB::Green;
+                break;
+            case 2:
+                leds[i] = CRGB::Blue;
+                break;
+        }
+    }
+}
+
 void setup() {
     wait_for_serial_connection(); // Optional, but seems to help Teensy out a
                                   // lot.
     Serial.println("   BLINK.INO - Simple LED Blink Example");
 
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-        .setCorrection(TypicalLEDStrip);
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);//.setCorrection(TypicalLEDStrip);
     FastLED.setBrightness(BRIGHTNESS);
+    nextColor(); // Initialize the first color
 }
 
+
 void loop() {
-    for (unsigned i = 0; i < NUM_LEDS; ++i) {
-        leds[i] = CRGB::Red;
-    }
-    FastLED.show();
-    delay(1000);
 
-    Serial.println(".");
+    EVERY_N_SECONDS( 1 ) { nextColor(); } // change color periodically
 
-    for (unsigned i = 0; i < NUM_LEDS; ++i) {
-        leds[i] = CRGB::Black;
-    }
     FastLED.show();
-    delay(1000);
 }
